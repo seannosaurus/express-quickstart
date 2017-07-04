f678cd5123303a37066cdd2ef62264086932b667
Sean <seannosaurus@gmail.com>
2017-07-04 19:49:10 +1000

    add helmet middleware

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const app = express();
 
+app.use('helmet');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/static', express.static(__dirname + '/client'));
 
